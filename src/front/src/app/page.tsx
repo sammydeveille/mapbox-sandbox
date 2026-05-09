193e3e5a5b0b8e00923fa73bd40657a8f31e2534
Sammy Deveille <sammydeveille@gmail.com>
2026-05-09 10:05:46 +0100

    node22

--- src/front/src/app/page.tsx
+++ src/front/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { AppShell } from '@/components/AppShell';
 import { Home } from '@/components/Home';
 
+export const dynamic = 'force-dynamic';
+
 export default function HomePage() {
   const mapboxToken = process.env.MAPBOX_ACCESS_TOKEN;
 
